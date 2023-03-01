63ac301b50a35034032725b1ef62374c5f12f7bb
Sai Pavan <saipavan@Sai-Pavan.local>
2023-03-01 16:39:41 -0500

    Initial commit

---
+++ src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/customer/v1/**").hasAuthority("customer")
+                .antMatchers("/api-docs/**").permitAll();
+    }
+
+}
